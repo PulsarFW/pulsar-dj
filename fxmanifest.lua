37713a286f7bce4989ed37d46a61e9b1ffe73775
BaskinB <54458253+BaskinB@users.noreply.github.com>
2026-05-02 14:23:05 -0500

    feat(server): added version checking.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,17 +1,17 @@
 name "DJBooth"
 author "katahtonic"
-version "v1.1"
+version "1.0.0"
 description 'DJBooth Edit By Katahtonic'
 fx_version "cerulean"
 game "gta5"
 
 client_scripts {
-	"@pulsar-polyzone/client.lua",
-	"@pulsar-polyzone/BoxZone.lua",
-	"@pulsar-polyzone/EntityZone.lua",
-	"@pulsar-polyzone/CircleZone.lua",
-	"@pulsar-polyzone/ComboZone.lua",
-    'client.lua'
+  "@pulsar-polyzone/client.lua",
+  "@pulsar-polyzone/BoxZone.lua",
+  "@pulsar-polyzone/EntityZone.lua",
+  "@pulsar-polyzone/CircleZone.lua",
+  "@pulsar-polyzone/ComboZone.lua",
+  'client.lua'
 }
 
 shared_script { 'config.lua' }
